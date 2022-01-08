e25debd140f00f86c83af4aed022b7f0cdd5659b
比嘉和樹 <higak@hikakazukinoMacBook-puro.local>
2022-01-08 17:27:06 +0900

    make ls

--- lsla.c
+++ lsla.c
@@ -12,9 +12,20 @@ struct dirent{
 }
 */
 int main(int argc, char**argv){
-    DIR *dp;
-    printf("%d parameters\n", argc);
-    for(int i=0;i<argc;i++){
-        printf("%s\n", argv[i]);
+    char *path = "./";
+    DIR *dir;
+    struct dirent *dent;
+    if(argc > 1){
+        path = argv[1];
     }
+    dir = opendir(path);
+    if (dir==NULL){
+        perror(path);
+        return 1;
+    }
+    while ((dent = readdir(dir))!= NULL){
+        printf("%s\n", dent->d_name);
+    }
+    closedir(dir);
+    return 0;
 }
